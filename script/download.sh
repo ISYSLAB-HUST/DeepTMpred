611dfa32e83e3f512092ef29d318ea6f1f9de6e3
wanglei <798306069@qq.com>
2021-08-13 15:17:11 +0800

    fix download urls

--- script/download.sh
+++ script/download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-orientaion_path="https://zenodo.org/record/5163061/files/deepTMpred-b.pth?download=1"
-deeptmpred_path="https://zenodo.org/record/5163061/files/orientaion-b.pth?download=1"
+orientaion_path="https://zenodo.org/record/5163061/files/deepTMpred-b.pth"
+deeptmpred_path="https://zenodo.org/record/5163061/files/orientaion-b.pth"
 mkdir model_files
 wget -P model_files "${orientaion_path}"
 wget -P model_files "${deeptmpred_path}"
