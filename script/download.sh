e5d5d4d4eb0789f9676c6430268d3cac3b48a7e7
wanglei <798306069@qq.com>
2021-08-31 15:59:19 +0800

    fix script download.sh

--- script/download.sh
+++ script/download.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 orientaion_path="https://zenodo.org/record/5163061/files/deepTMpred-b.pth"
 deeptmpred_path="https://zenodo.org/record/5163061/files/orientaion-b.pth"
-mkdir model_files
+if [ ! -d "model_files" ]; then
+  mkdir model_files
+fi
 wget -P model_files "${orientaion_path}"
 wget -P model_files "${deeptmpred_path}"
 echo "all model files are downloaded successfully!"
